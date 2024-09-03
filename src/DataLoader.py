9798b979a340f489d1641c57882bf16d1ce9696f
ubuntu <ubuntu@localhost.localdomain>
2024-09-04 00:39:15 +0800

    init

---
+++ src/DataLoader.py
@@ -0,0 +1,15 @@
+#%%
+import pandas as pd
+
+#%%
+class 
+def func():
+    print(1)
+
+#%%
+b = pd.read_parquet("/home/ubuntu/data/factor/JIE_factor_5/JIE_factor_5.v1.parquet")
+print(1)
+#%%
+if __name__ == "__main__":
+    a = 1
+    func()
