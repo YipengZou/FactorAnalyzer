3a653003bcd56c0475704066e734e33a19d367fd
Yip <yipengzou@foxmail.com>
2024-09-04 00:48:07 +0800

    upd

--- src/DataLoader.py
+++ src/DataLoader.py
@@ -2,9 +2,15 @@
 import pandas as pd
 
 #%%
-class 
+class FactorLoader:
+    def __init__(self) -> None:
+        pass
+
+    def set_params(self) -> None:
+        pass
+
 def func():
-    print(1)
+    print(12)
 
 #%%
 b = pd.read_parquet("/home/ubuntu/data/factor/JIE_factor_5/JIE_factor_5.v1.parquet")
